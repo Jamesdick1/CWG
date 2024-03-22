58246c0d2a483a71beb25cd12d0f836ee89380c5
James D <75795346+Jamesdick1@users.noreply.github.com>
2024-03-21 20:51:01 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,85 @@
+//section 1
+function displayText() {
+    var text = document.getElementById("#1");
+    text.style.display = "block";
+          
+          var text = document.getElementById("drink");
+    text.style.display = "inline";
+          
+          var text = document.getElementById("quiet");
+    text.style.display = "inline";
+  
+        var text = document.getElementById("intro");
+    text.style.display = "none";
+  
+          var text = document.getElementById("begin");
+    text.style.display = "none";
+  
+           
+          
+  }
+  //section 2a
+      function introduce() {
+       var text = document.getElementById("#2a");
+    text.style.display = "inline";
+  
+          var text = document.getElementById("gowith");
+    text.style.display = "inline";
+  
+              var text = document.getElementById("gohome");
+    text.style.display = "inline";
+  
+            var text = document.getElementById("#1");
+    text.style.display = "none";
+  
+           var text = document.getElementById("drink");
+    text.style.display = "none";
+  
+                 var text = document.getElementById("quiet");
+    text.style.display = "none";
+  
+  
+          }
+  //
+          function quiet() {
+       var text = document.getElementById("#2b");
+    text.style.display = "inline";
+              
+    var text = document.getElementById("attack");
+    text.style.display = "inline";
+
+    var text = document.getElementById("ask");
+    text.style.display = "inline";
+              
+            var text = document.getElementById("#1");
+    text.style.display = "none";
+  
+           var text = document.getElementById("drink");
+    text.style.display = "none";
+  
+                 var text = document.getElementById("quiet");
+    text.style.display = "none";
+  
+  
+          }
+      function decline(){
+        var text = document.getElementById("#3a");
+    text.style.display = "inline";
+
+    var text = document.getElementById("continue");
+    text.style.display = "inline";
+  
+              var text = document.getElementById("gohome");
+    text.style.display = "none";
+  
+
+        var text = document.getElementById("#2a");
+        text.style.display = "none";
+
+        var text = document.getElementById("gowith");
+    text.style.display = "none";
+  
+              var text = document.getElementById("gohome");
+    text.style.display = "none";
+  
+      }
